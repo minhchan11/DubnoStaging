ab1ee8dc80b62e66a71f5484c65ad2313d868d4a
Jessica Glenn <glenj392@newschool.edu>
2017-06-05 13:49:41 -0700

    push project to github for help in fixing error when trying to send email from /email/sendemail

---
+++ src/Dubno/Controllers/EmailController.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Dubno.Models;
+using Microsoft.AspNetCore.Hosting;
+
+namespace Dubno.Controllers
+{
+    public class EmailController : Controller
+    {
+
+        private IHostingEnvironment _env;
+
+        public EmailController(IHostingEnvironment env)
+        {
+            _env = env;
+        }
+
+        public IActionResult SendEmail()
+        {
+            return View();
+        }
+
+        public IActionResult SentEmail()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> SendEmail(EmailViewModel model)
+        {
+            var emailTemplate = "WelcomeEmail";
+            var emailSubject = "Welcome to our site.";
+            var message = EMailTemplate(emailTemplate);
+            message = message.Replace("@ViewBag.Name", model.Username);
+            MessageServices msg = new MessageServices();
+            await msg.SendEmailAsync(model.Username, model.Email, emailSubject, message, model.Attachments);
+            ModelState.AddModelError("", "Email successfully sent.");
+            return View("EmailSent");
+        }
+
+
+        [HttpPost]
+        public async Task<ActionResult> SendBulkEmail(BulkEmailViewModel model)
+        {
+            var emailTemplate = "WelcomeEmail";
+            var emailSubject = "Welcome to our site.";
+            var message = EMailTemplate(emailTemplate);
+            message = message.Replace("@ViewBag.Name", model.Username);
+            MessageServices msg = new MessageServices();
+            await msg.SendBulkEmailAsync(model.Username, model.Email, emailSubject, message, model.Attachments);
+            ModelState.AddModelError("", "Email successfully sent.");
+            return View("BulkEmail");
+        }
+
+        public string EMailTemplate(string template)
+        {
+            string body = null;
+            var templateFilePath = _env.WebRootPath + "\\templates\\" + template + ".cshtml";
+            body = System.IO.File.ReadAllText(templateFilePath);
+            return body;
+        }
+    }
+}
---
+++ src/Dubno/ViewModels/EmailViewModel.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Dubno.Models
+{
+    public class EmailViewModel
+    {
+        [Required]
+        [Display(Name = "Username")]
+        public string Username { get; set; }
+
+        [Required]
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+
+        [Display(Name = "Attachment")]
+        public ICollection<IFormFile> Attachments { get; set; }
+    }
+
+    public class BulkEmailViewModel
+    {
+        [Required]
+        [Display(Name = "Username")]
+        public string Username { get; set; }
+
+        [Required]
+        [Display(Name = "Email")]
+        public string[] Email { get; set; }
+
+        [Display(Name = "Attachment")]
+        public ICollection<IFormFile> Attachments { get; set; }
+    }
+}
